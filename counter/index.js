3f1ed3f088d94f826dd1b1a6e5ebc4c1f2c02648
Danilocl <danilocalessa@gmail.com>
2024-06-27 17:54:50 -0300

    subindo projetos

---
+++ counter/index.js
@@ -0,0 +1,42 @@
+const decrease = document.getElementById("decrease")
+const reset = document.getElementById("reset")
+const increase = document.getElementById("increase")
+const count = document.getElementById("count")
+
+let interval;
+
+//intervalo de 100ms entre as adições
+const startInterval = (operation) => {
+    interval = setInterval(operation, 100);
+}
+
+const stopInterval = () => {
+    clearInterval(interval)
+}
+
+
+reset.addEventListener('click', () => {
+    count.textContent = 0
+})
+
+
+increase.addEventListener('mousedown', () => {
+    startInterval(() => {
+        count.textContent++
+    })
+})
+
+increase.addEventListener('mouseup', stopInterval)
+increase.addEventListener('mouseleave', stopInterval)
+
+
+decrease.addEventListener('mousedown', () => {
+    startInterval(() => {
+        count.textContent--
+    })
+})
+
+decrease.addEventListener('mouseup', stopInterval)
+decrease.addEventListener('mouseleave', stopInterval)
+
+
