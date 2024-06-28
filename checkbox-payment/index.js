dc10ef37df4154a34dbae2f3a338f7940e30a2ff
Danilocl <danilocalessa@gmail.com>
2024-06-28 19:31:34 -0300

    checkbox

---
+++ checkbox-payment/index.js
@@ -0,0 +1,25 @@
+const payOptions = document.querySelectorAll(".container .payOptions input")
+const subscribe = document.getElementById("subscribeBtn")
+const submit = document.getElementById("submit")
+
+submit.addEventListener('click', () => {
+    let isPaySelected = false
+
+    payOptions.forEach(input => {
+        if (input.checked) {
+            isPaySelected = true
+        }
+    })
+
+    if (!isPaySelected) {
+        window.alert("Please select a payment option")
+    }
+
+    if (!subscribe.checked) {
+        window.alert("Please subscribe")
+    } else if (isPaySelected) {
+        window.alert("Subscribed!!")
+    }
+
+
+})
